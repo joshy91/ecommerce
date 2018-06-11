6b128b9b188e20e689a552ebd2c4257019d51673
joshy91 <joshua.davidsonsga@gmail.com>
2018-06-11 17:42:22 -0500

    Add storefront controller and format all items page, also add modals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'storefront#all_items'
+  get 'categorical' => 'storefront#by_category'
+  get 'branding' => 'storefront#by_brand'
   resources :categories
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

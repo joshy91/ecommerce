8b0fdc7eb4fdf8809fc97ba339bfaed6f0a75f9d
joshy91 <joshua.davidsonsga@gmail.com>
2018-06-11 15:28:05 -0500

    Add Image uploader and imageMagick gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_194516) do
+ActiveRecord::Schema.define(version: 2018_06_11_201634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_06_11_194516) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
